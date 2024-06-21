81a69b01b43bd6bf801a7e5a51d838bfd8b02ed9
Your Name <your.email@example.com>
2024-06-21 18:08:21 +0530

    hari

--- script.js
+++ script.js
@@ -522,3 +522,21 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+
+
+
+
+
+
+
+document.querySelectorAll('.bottom-icons a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
